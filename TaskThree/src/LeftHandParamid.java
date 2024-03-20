05a4c6d85743782194dfb615561fe0391a40378a
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2024-03-20 14:03:45 +0530

    Initial commit

---
+++ TaskThree/src/LeftHandParamid.java
@@ -0,0 +1,10 @@
+public class LeftHandParamid {
+    public static void main(String[] args) {
+        //"" er vitore space hole oitar value ase java te
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
